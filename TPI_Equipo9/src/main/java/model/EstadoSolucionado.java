6dcd208a2e2c7ed8d3321bcc454b16148cd3c6bc
FabreFacundo <facufabre2011@gmail.com>
2023-11-29 15:58:07 -0300

    actualizado

--- TPI_Equipo9/src/main/java/model/EstadoSolucionado.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class EstadoSolucionado extends Estado{
-
-	public void cheakearEstado(Incidente incidenteActual){}
-	public boolean cumpleCondicion(){};
-
-}
--- TPI_Equipo9/src/main/java/repository/ProblemaDAO.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Problema;
-
-public class ProblemaDAO extends DAO<Problema> {
-    public ProblemaDAO() {
-        setClase(Problema.class);
-    }
-}
